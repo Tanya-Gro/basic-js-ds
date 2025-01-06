3b16e65424604abe1947a2229808330219d951ad
unknown <tarpel.gta@gmail.com>
2025-01-07 00:22:34 +0200

    add: BinarySearchTree solution

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,36 +1,70 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
-
+  constructor(data = null) {
+    this.head = data;
+  }
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.head;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    const addNode = function(node, data) {
+      if (!node) return new Node(data);
+      if(node.data === data) return node;
+      if(data < node.data) node.left = addNode(node.left, data);
+      else node.right = addNode(node.right, data);
+      return node;
+    }
+    this.head = addNode(this.head, data);
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    return !!this.find(data);
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    const findNode = function (node, data) {
+      if (!node) return null;
+      if(node.data === data) return node;
+      if(data < node.data) return findNode(node.left, data);
+      else return findNode(node.right, data);
+    }
+    return findNode(this.head, data);
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    const removeNode = function (node, data) {
+      if (!node) return null;
+      if(data < node.data) {
+        node.left = removeNode(node.left, data);
+        return node;
+      } else if(data > node.data) {
+        node.right = removeNode(node.right, data);
+        return node;
+      } else if(!node.left && !node.right) return null;
+      if (!node.left) {
+        node = node.right;
+        return node;
+      }
+      if (!node.right) {
+        node = node.left;
+        return node;
+      }
+      let minFromRight = node.right;
+      while(minFromRight.left)
+        minFromRight = minFromRight.left;
+      node.data = minFromRight.data;
+      node.right = removeNode(node.right, node.data);
+      return node;
+    }
+    this.head = removeNode(this.head, data);
   }
 
   min() {
